fda6a4c95fe29e70294166dca9c111c7092011ea
Maulana Aji Satrio <maulana.satrio@flip.id>
2024-10-28 20:46:57 +0700

    add span in middleware

--- internal/delivery/http/middleware/jwt.go
+++ internal/delivery/http/middleware/jwt.go
@@ -8,6 +8,8 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/masatrio/bookstore-api/config"
 	"github.com/masatrio/bookstore-api/utils"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type contextKey string
@@ -17,14 +19,18 @@ const userIDKey contextKey = "userID"
 // JWTMiddleware checks the validity of the JWT token in the Authorization header.
 func JWTMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx, span := trace.SpanFromContext(r.Context()).TracerProvider().Tracer("").Start(r.Context(), "JWTMiddleware")
+		defer span.End()
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
+			span.SetStatus(codes.Error, "Authorization header missing")
 			http.Error(w, "Authorization header missing", http.StatusUnauthorized)
 			return
 		}
 
 		bearerToken := strings.Split(authHeader, "Bearer ")
 		if len(bearerToken) != 2 {
+			span.SetStatus(codes.Error, "Invalid Authorization header format")
 			http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
 			return
 		}
@@ -40,14 +46,16 @@ func JWTMiddleware(next http.Handler) http.Handler {
 		})
 
 		if err != nil || !token.Valid {
+			span.SetStatus(codes.Error, "Invalid or expired token")
 			http.Error(w, "Invalid or expired token", http.StatusUnauthorized)
 			return
 		}
 
 		if claims, ok := token.Claims.(*utils.Claims); ok && token.Valid {
-			ctx := context.WithValue(r.Context(), userIDKey, claims.UserID)
+			ctx = context.WithValue(r.Context(), userIDKey, claims.UserID)
 			r = r.WithContext(ctx)
 		} else {
+			span.SetStatus(codes.Error, "Invalid token claims")
 			http.Error(w, "Invalid token claims", http.StatusUnauthorized)
 			return
 		}
